f8c94dcb4149f99d879a92af822ff963e619d779
rachid <olgor@hotmail.com>
2019-09-08 14:40:23 +0200

    router added to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 const app = express();
+
+import indexRoutes from './routes/index';
 
 //Settings
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(morgan('dev'));
+
+//Routes
+app.use('/api', indexRoutes);
+
+//strore public files
+app.use('./public', express.static(path.resolve('public')));
 
 export default app;
