ce8fc5b7ded9f4700368ac2523f807c5359048fc
rachid <olgor@hotmail.com>
2019-09-08 14:24:48 +0200

    morgan added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
 
 export default app;
