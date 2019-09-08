340e01a7fba126831cb6d95082570ae34cf821ef
rachid <olgor@hotmail.com>
2019-09-08 13:44:23 +0200

    nodemon added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,4 +2,7 @@ import express from 'express'
 
 const app = express();
 
+//Settings
+app.set('port', process.env.PORT || 3000);
+
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import app from './app';
 
-app.listen(3000, () => {
-    console.log('Server on port ', 3000);
-})
+async function main() {
+    await app.listen(app.get('port'));
+    console.log('Server on port ', app.get('port'));
+
+}
+
+main();
