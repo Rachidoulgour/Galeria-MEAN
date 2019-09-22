bb82f19c7a63e51636cf3e77b0de43f7a0d7a3e9
rachid <olgor@hotmail.com>
2019-09-22 23:34:21 +0200

    form component confgured ti upload photo

--- frontend/src/app/components/photo-form/photo-form.component.ts
+++ frontend/src/app/components/photo-form/photo-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
   styleUrls: ['./photo-form.component.scss']
 })
 export class PhotoFormComponent implements OnInit {
-
+file: File;
+photoSelected: string | ArrayBuffer;
   constructor() { }
 
   ngOnInit() {
+  }
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if (event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
   }
 
 }
