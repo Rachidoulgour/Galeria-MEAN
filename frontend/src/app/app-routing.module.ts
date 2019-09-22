1f5ec9eb258b4650f63cc50ebfda2cd00d020c92
rachid <olgor@hotmail.com>
2019-09-22 13:04:19 +0200

    routing configured

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PhotoListComponent} from './components/photo-list/photo-list.component'
+import {PhotoFormComponent} from './components/photo-form/photo-form.component';
+import {PhotoPreviewComponent} from './components/photo-preview/photo-preview.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'public',
+    component: PhotoListComponent
+  },
+  {
+    path:'public/new',
+    component: PhotoFormComponent
+  },
+  {
+    path: 'public/:id',
+    component: PhotoPreviewComponent
+  },
+  {
+    path: '',
+    redirectTo: '/public',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
